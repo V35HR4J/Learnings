544ceb27a89b005fe6fa24cda760802362957d51
Veshraj Ghimire <54109630+V35HR4J@users.noreply.github.com>
2021-01-24 10:03:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-Just Learning Things!
-<a href="https://veshjs.herokuapp.com/GuessGame">Click Here To go Guess Game</a>
-<a href="http://veshjs.herokuapp.com/TableCalculator">Click Here To go Guess Game</a>
+Just Learning Things!<br>
+<a href="https://veshjs.herokuapp.com/GuessGame">Click Here To go Guess Game</a><br>
+<a href="http://veshjs.herokuapp.com/TableCalculator">Click Here To go Guess Game</a><br>
